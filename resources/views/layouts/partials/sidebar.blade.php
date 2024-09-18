44cf904209ff6ebee8a03ad42a956401be5101de
Ozodbek Xayrullayevich <ozod.shukurov@gmail.com>
2024-09-18 16:19:20 +0500

    AdminLte UI

---
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -0,0 +1,121 @@
+<!-- Sidebar -->
+<div class="sidebar">
+    <!-- Sidebar user panel (optional) -->
+    <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+            <img src="{{\Illuminate\Support\Facades\Vite::asset('resources/img/user2-160x160.jpg')}}" class="img-circle elevation-2" alt="User Image">
+        </div>
+        <div class="info">
+            <a href="#" class="d-block">Admin</a>
+        </div>
+    </div>
+
+    <!-- SidebarSearch Form -->
+    <div class="form-inline">
+        <div class="input-group" data-widget="sidebar-search">
+            <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
+            <div class="input-group-append">
+                <button class="btn btn-sidebar">
+                    <i class="fas fa-search fa-fw"></i>
+                </button>
+            </div>
+        </div>
+    </div>
+
+    <!-- Sidebar Menu -->
+    <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+            <!-- Add icons to the links using the .nav-icon class
+                 with font-awesome or any other icon font library -->
+
+            <li class="nav-item">
+                <a href="pages/widgets.html" class="nav-link active">
+                    <i class="nav-icon fas fa-tachometer-alt"></i>
+                    <p>
+                        Панель управления
+                    </p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <i class="nav-icon fas fa-users"></i>
+                    <p>
+                        Сотрудники
+                        <i class="right fas fa-angle-left"></i>
+                    </p>
+                </a>
+                <ul class="nav nav-treeview" style="display: none;">
+                    <li class="nav-item">
+                        <a href="../../index.html" class="nav-link">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>Dashboard v1</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="../../index2.html" class="nav-link">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>Dashboard v2</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="../../index3.html" class="nav-link">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>Dashboard v3</p>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+            <li class="nav-item">
+                <a href="pages/widgets.html" class="nav-link">
+                    <i class="nav-icon fas fa-user-injured"></i>
+                    <p>
+                        Пациент
+                    </p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="pages/widgets.html" class="nav-link">
+                    <i class="nav-icon fas fa-user-alt"></i>
+                    <p>
+                        Техник
+                    </p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="pages/widgets.html" class="nav-link">
+                    <i class="nav-icon fas fa-calendar-check"></i>
+                    <p>
+                        Приём пациента
+                    </p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="pages/widgets.html" class="nav-link">
+                    <i class="nav-icon fas fa-chart-area"></i>
+                    <p>
+                        Отчеты
+                    </p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="pages/widgets.html" class="nav-link">
+                    <i class="nav-icon fas fa-money-bill"></i>
+                    <p>
+                        Платежи
+                    </p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="pages/widgets.html" class="nav-link">
+                    <i class="nav-icon fas fa-home"></i>
+                    <p>
+                        Склад
+                    </p>
+                </a>
+            </li>
+
+        </ul>
+    </nav>
+    <!-- /.sidebar-menu -->
+</div>
+<!-- /.sidebar -->
